5bf3eb3d71847c8468e8431a556937965c256f02
ceronhermilo <ceronhermilo@yahoo.com>
2021-03-12 14:36:11 -0600

    After installing redux-thunk

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import axios from 'axios';
 import React,{Component} from 'react';
 import {Route,Link,withRouter} from 'react-router-dom';
+import { connect} from 'react-redux';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={
+  // constructor(props){
+  //   super(props);
+  //   this.state={
 
-    }
-  }
+  //   }
+  // }
   render(){
     return (
       <div className="App">
@@ -19,4 +20,15 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps=(state)=>{
+  return {
+
+  }
+};
+const mapDispachToProps=(dispach)=>{
+  return {
+
+  }
+};
+
+export default connect(mapStateToProps,mapDispachToProps)(App);
